4fd186a27f7ab9ab2d18fc8168d8e2ee83e57413
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-07-07 22:18:32 +0100

    ft(dashboard):goal card
    - add markup
    
    - add style
    
    [Finishes #173669895]

--- src/components/Elements/Dashboard/Goals/index.jsx
+++ src/components/Elements/Dashboard/Goals/index.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 import Card from '../../../UI/Card';
 import plusIcon from '../../../../assets/images/plus.svg';
+import deleteIcon from '../../../../assets/images/deleteIcon.svg';
+import markIcon from '../../../../assets/images/markIcon.svg';
+import moreIcon from '../../../../assets/images/moreIcon.svg';
+import pauseIcon from '../../../../assets/images/pauseIcon.svg';
+import penIcon from '../../../../assets/images/penIcon.svg';
+import GoalCard from '../../../UI/GoalCard';
+
 import './Goals.scss';
 
 const Goals = (props) => {
@@ -9,12 +16,26 @@ const Goals = (props) => {
     <div className='row body'>
       <div className='col-l-12 dropdown__navigation__area'></div>
       <div className='row main__content__area'>
-        <Card classname='col-l-4 add__goals__card'>
+        <Card classname='add__goals__card'>
           <button type='submit' className='add__goal__button'>
             <img src={plusIcon} alt='plus icon' className='plus__icon' />
             <h5>New Goal</h5>
           </button>
         </Card>
+        <GoalCard
+          goal='New Car'
+          deadLine='Sep 30, 2020'
+          rate='4.00%'
+          progress='5%'
+          targetFraction='$200,000.00/$500,000.00'
+        />
+        <GoalCard
+          goal='New Car'
+          deadLine='Sep 30, 2020'
+          rate='4.00%'
+          progress='5%'
+          targetFraction='$200,000.00/$500,000.00'
+        />
       </div>
     </div>
   );
