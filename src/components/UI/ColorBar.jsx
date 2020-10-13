b06e40be193660470416eb7a38ceb0ca2ae42461
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-10-14 00:20:46 +0100

    add color pallete

--- src/components/UI/ColorBar.jsx
+++ src/components/UI/ColorBar.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ColorBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 1.6rem;
+  height: 1.6rem;
+  border-radius: 0.8rem;
+  position: relative;
+  margin: ${props=>props.margin ||0};
+  cursor: pointer;
+  background-color: ${props => props.backgroundColor || 'white'};
+
+  .bar__icon {
+    width: 0.8rem;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .mark {
+    width: 0.8rem;
+  }
+`;
 
 const ColorBar = (props) => {
-  const { color, className = '', children, handleClick = () => null } = props;
+  const { color, children, handleClick = () => null } = props;
   return (
-    <div
-      className={`color__bar__item ${className}`}
-      style={{ backgroundColor: color }}
-      onClick={handleClick}
-      {...props}
-    >
+    <ColorBox backgroundColor={color} onClick={handleClick} {...props}>
       {children}
-    </div>
+    </ColorBox>
   );
 };
 
--- src/components/UI/CustomButton.jsx
+++ src/components/UI/CustomButton.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import Button from './Button';
 import './Styles/CustomButton.scss';
 
-const CustomButton = ({ title = '', handleClick = () => null }) => {
+const Button = styled.button`
+  color: ${props=>props.color || 'white'};
+  padding: ${props=>props.paddingTopDown || '0.9rem'} ${props=>props.paddindLeftRight||'3rem'};
+  margin: ${props => props.marginTopDown || '1rem'} ${props => props.marginLeftRight || 0};
+  border: none;
+  text-transform: uppercase;
+  border-radius: ${props=>props.borderRadius||'1rem'};
+  background: ${props=>props.background||'#347af0'};
+  cursor: pointer;
+  outline: none;
+`;
+
+const CustomButton = (props) => {
+  const { title = '', handleClick = () => null } = props
   return (
-    <Button type='submit' className='custom__button' onClick={handleClick}>
+    <Button type='submit' onClick={handleClick} {...props}>
       {title}
     </Button>
   );
