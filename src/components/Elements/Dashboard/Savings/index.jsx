7177beed80b1d17537fc8a4315ffcdc976142f7d
Ayebakuro <xplicitkuro@gmail.com>
2021-04-22 14:15:27 +0100

    Savings Module Complete

--- src/components/Elements/Dashboard/Savings/index.jsx
+++ src/components/Elements/Dashboard/Savings/index.jsx
@@ -1,90 +1,232 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Calendar from "../../../Reusable/Calendar";
 import WorthCard from "../../../Reusable/WorthCard";
 import SavingsIcon from "../../../UI/Icons/Savings";
-import FloatingButton from "../../../Reusable/FloatingButton";
-import addIcon from "../../../../assets/images/addIcon.svg";
-import cancelIcon from "../../../../assets/images/cancelIcon.svg";
 import TableHead from "../../../Reusable/TableHead";
 import TableData from "../../../Reusable/TableData";
-import FloatingButtonContent from "../../../Reusable/FloatingButtonContent";
-import ContentWrapper from "../../../Reusable/FloatingButtonContentWrapper";
 import "./Savings.scss";
 // import Modal from "../../../Reusable/Modal/Modal";
 import SavingsForm from "../SavingsForm";
 import ModalUI from "../../../Reusable/ModalUI";
+import {
+  Box,
+  Fab,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Menu,
+  MenuItem,
+  useTheme,
+} from "@material-ui/core";
+import { Add, ArrowDownward, ArrowUpward, Close } from "@material-ui/icons";
+import { green, red } from "@material-ui/core/colors";
+import ApiCall from "../../../../helper/Api";
+import { toast } from "react-toastify";
+import convertToCurrency from "../../../../helper/convertToCurrency";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  flex: {
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+    [theme.breakpoints.up("sm")]: {
+      flexDirection: "row",
+    },
+    justifyContent: "space-between",
+    marginTop: "0.5rem",
+  },
+  header: {
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "75%",
+    },
+  },
+  worth: {
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      marginTop: "0.5rem",
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "25%",
+      marginLeft: "0.5rem",
+    },
+  },
+  fab: {
+    position: "absolute",
+    [theme.breakpoints.down("sm")]: {
+      bottom: theme.spacing(3),
+      right: theme.spacing(3),
+    },
+    [theme.breakpoints.up("md")]: {
+      bottom: theme.spacing(5),
+      right: theme.spacing(5),
+    },
+  },
+  iconGreen: { color: green[500] },
+  iconRed: { color: red[500] },
+}));
 
 const Savings = (props) => {
-  const [showFloatingContent, setShowFloatingContent] = useState(false);
   const [displayMore, setDisplayMore] = useState(false);
   const [displayIncomeTable, setDisplayIncomeTable] = useState(false);
   const [displayExpensesTable, setDisplayExpensesTable] = useState(false);
   const [clickedItem, setClickedItem] = useState("Income");
   const [displayModal, setDisplayModal] = useState(false);
   const [date, setDate] = useState({ year: "", month: "" });
-
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [transactions, setTransactions] = useState([]);
+  const theme = useTheme();
+  const classes = useStyles(theme);
   const handleSetDisplayContent = (item) => {
     setClickedItem(item);
     return setDisplayModal(!displayModal);
   };
-  // const handleDisplayModalContent = () => {
-  //   console.log("display");
-  //   let modalContent;
-  //   if (clickedItem === "income") {
-  //     modalContent = (
-  //       <SavingsForm
-  //         cardTitle="New Income"
-  //         type="income"
-  //         toggleModal={setDisplayModal(!displayModal)}
-  //       />
-  //     );
-  //   } else {
-  //     modalContent = (
-  //       <SavingsForm
-  //         cardTitle="New Expense"
-  //         type="expense"
-  //         toggleModal={setDisplayModal(!displayModal)}
-  //       />
-  //     );
-  //   }
-  //   return modalContent;
-  // };
-  console.log(date);
-  return (
-    <div className="savings">
-      <div className="col-l-10">
-        <div className="row calender__wrapper">
-          <div className="col-l-4 ">
-            <Calendar handleReturnSelectedMonth={setDate} />
-          </div>
-        </div>
-        <div className="row savings__details__area">
-          <div className="col-l-8 savings__table">
-            <div className="savings__table__wrapper">
-              <TableHead
-                toggleCaret={displayIncomeTable}
-                handleShowTable={() =>
-                  setDisplayIncomeTable(!displayIncomeTable)
-                }
-              />
 
-              {displayIncomeTable ? (
-                <div className="table__body">
-                  <TableData
-                    displayMore={displayMore}
-                    handleClickMore={() => setDisplayMore(!displayMore)}
-                  />
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
+  /**
+   * Open Menu
+   * @param {*} e
+   */
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  /**
+   * Close Menu
+   */
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const menuId = "fabMenu";
+
+  const fabMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "bottom", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleClose}
+    >
+      <MenuItem
+        onClick={() => {
+          handleSetDisplayContent("income");
+          handleClose();
+        }}
+      >
+        <ListItemIcon className={classes.iconGreen}>
+          <ArrowDownward />
+        </ListItemIcon>
+        <ListItemText>Add Income</ListItemText>
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          handleSetDisplayContent("expense");
+          handleClose();
+        }}
+      >
+        <ListItemIcon className={classes.iconRed}>
+          <ArrowUpward />
+        </ListItemIcon>
+        <ListItemText>Add Expense</ListItemText>
+      </MenuItem>
+    </Menu>
+  );
+
+  useEffect(() => {
+    ApiCall.getCall("transaction").then(
+      (res) => {
+        console.log(res.payload);
+        setTransactions(res.payload);
+      },
+      (err) => {
+        toast.error("Unable to get Transactions");
+      }
+    );
+  }, []);
+
+  const getTransactions = () => {
+    ApiCall.getCall("transaction").then(
+      (res) => {
+        setTransactions(res.payload);
+      },
+      (err) => {
+        toast.error("Unable to get Transactions");
+      }
+    );
+  };
+
+  const incomeTransactions = transactions.filter(
+    (trx) => trx.type === "income"
+  );
+  const expenseTransactions = transactions.filter(
+    (trx) => trx.type === "expense"
+  );
+
+  const incomeTrx = incomeTransactions.map((trax, i) => (
+    <TableData
+      key={i}
+      dataTitle={trax.description}
+      amount={convertToCurrency(trax.amount)}
+      displayMore={displayMore}
+      handleClickMore={() => setDisplayMore(!displayMore)}
+    />
+  ));
+  const expenseTrx = expenseTransactions.map((trax, i) => (
+    <TableData
+      key={i}
+      dataTitle={trax.description}
+      amount={convertToCurrency(trax.amount)}
+      displayMore={displayMore}
+      handleClickMore={() => setDisplayMore(!displayMore)}
+    />
+  ));
+  const expenseTotal = expenseTransactions.reduce(
+    (total, curr) => total + curr.amount,
+    0
+  );
+
+  const incomeTotal = incomeTransactions.reduce(
+    (total, curr) => total + curr.amount,
+    0
+  );
+
+  return (
+    <>
+      <Box className={classes.root}>
+        <Box>
+          <Calendar handleReturnSelectedMonth={setDate} />
+        </Box>
+        <Box className={classes.flex}>
+          <Box className={classes.header}>
+            <TableHead
+              color={"green"}
+              toggleCaret={displayIncomeTable}
+              secondTitle={convertToCurrency(incomeTotal)}
+              handleShowTable={() => setDisplayIncomeTable(!displayIncomeTable)}
+            />
+
+            {displayIncomeTable ? (
+              <div className="table__body">{incomeTrx}</div>
+            ) : (
+              ""
+            )}
             <div className="income__wrapper">
               <TableHead
+                color="red"
                 headerWrapperClassName="expenditure__table"
-                firstTitle="Expenditure"
-                secondTitle="6,365.87"
+                firstTitle="Expense"
+                secondTitle={convertToCurrency(expenseTotal)}
                 toggleCaret={displayExpensesTable}
                 handleShowTable={() =>
                   setDisplayExpensesTable(!displayExpensesTable)
@@ -92,79 +234,60 @@ const Savings = (props) => {
               />
               <div className="table__body">
                 {displayExpensesTable ? (
-                  <div className="table__body">
-                    <TableData
-                      displayMore={displayMore}
-                      handleClickMore={() => setDisplayMore(!displayMore)}
-                    />
-                  </div>
+                  <div className="table__body">{expenseTrx}</div>
                 ) : (
                   ""
                 )}
               </div>
             </div>
-          </div>
-          <div className="col-l-3 savings__details__card">
+          </Box>
+          <Box className={classes.worth}>
             <WorthCard
               cardTitle="MY SAVINGS"
               className="savigns__card"
-              amountToDisPlay="2,0050"
+              amountToDisPlay={convertToCurrency(incomeTotal - expenseTotal)}
               iconClassName="savings__icon"
               Icon={SavingsIcon}
               iconColor="#000000"
             />
-          </div>
-        </div>
-      </div>
-      <div className="floating__action">
-        {showFloatingContent && (
-          <div className="content">
-            <FloatingButtonContent
-              onClick={() => {
-                handleSetDisplayContent("income");
-              }}
-            >
-              <ContentWrapper buttonName="Income">+</ContentWrapper>
-            </FloatingButtonContent>
-            <FloatingButtonContent
-              onClick={() => {
-                handleSetDisplayContent("expense");
-              }}
-            >
-              <ContentWrapper
-                buttonName="Expenses"
-                extraClassName="add__expense"
-              >
-                -
-              </ContentWrapper>
-            </FloatingButtonContent>
-
-            <ModalUI
-              onOpen={displayModal}
-              onClose={() => setDisplayModal(!displayModal)}
-              title={clickedItem === "income" ? "New Income" : "New Expense"}
-            >
-              {clickedItem === "income" ? (
-                <SavingsForm
-                  type={clickedItem}
-                  onClose={() => setDisplayModal(!displayModal)}
-                />
-              ) : (
-                <SavingsForm
-                  type={clickedItem}
-                  onClose={() => setDisplayModal(!displayModal)}
-                />
-              )}
-            </ModalUI>
-          </div>
+          </Box>
+        </Box>
+      </Box>
+      <Fab
+        className={classes.fab}
+        color="primary"
+        aria-label="Add"
+        onClick={handleClick}
+      >
+        {isMenuOpen ? <Close /> : <Add />}
+      </Fab>
+      {fabMenu}
+      <ModalUI
+        onOpen={displayModal}
+        onClose={() => {
+          setDisplayModal(!displayModal);
+        }}
+        title={clickedItem === "income" ? "New Income" : "New Expense"}
+      >
+        {clickedItem === "income" ? (
+          <SavingsForm
+            type={clickedItem}
+            onClose={() => {
+              setDisplayModal(!displayModal);
+              getTransactions();
+            }}
+          />
+        ) : (
+          <SavingsForm
+            type={clickedItem}
+            onClose={() => {
+              setDisplayModal(!displayModal);
+              getTransactions();
+            }}
+          />
         )}
-        <FloatingButton
-          onClick={() => setShowFloatingContent(!showFloatingContent)}
-        >
-          <img src={showFloatingContent ? cancelIcon : addIcon} alt="icon" />
-        </FloatingButton>
-      </div>
-    </div>
+      </ModalUI>
+    </>
   );
 };
 
--- src/components/Reusable/WorthCard/index.jsx
+++ src/components/Reusable/WorthCard/index.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import { makeStyles } from "@material-ui/core";
+import React from "react";
 
-import carat from '../../../assets/images/up-carat.svg';
-import Card from '../../UI/Card';
-import './worthcard.scss';
+import carat from "../../../assets/images/up-carat.svg";
+import Card from "../../UI/Card";
+import "./worthcard.scss";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+  },
+}));
 const WorthCard = ({
   cardTitle,
   className,
   amountToDisPlay,
-  iconButtonClassName = '',
+  iconButtonClassName = "",
   iconClassName,
   Icon,
   iconColor,
 }) => {
+  const classes = useStyles();
   return (
-    <Card classname={`card ${className}`}>
-      <div className='savings__details'>
-        <div className='details'>
-          <h6 className='my__savings'>{cardTitle || 'MY NET WORTH'}</h6>
-          <p className='amount'> {`$ ${amountToDisPlay || '2,149.00'}`}</p>
+    <Card classname={`card ${className}, ${classes.root}`}>
+      <div className="savings__details">
+        <div className="details">
+          <h6 className="my__savings">{cardTitle || "MY NET WORTH"}</h6>
+          <p className="amount"> {`${amountToDisPlay || "2,149.00"}`}</p>
         </div>
-        <div className='icon'>
+        <div className="icon">
           <div className={`icon__wrapper ${iconButtonClassName}`}>
-            <Icon fill={iconColor || '#ffffff'} className={iconClassName} />
+            <Icon fill={iconColor || "#ffffff"} className={iconClassName} />
           </div>
         </div>
       </div>
-      {className === 'savigns__card' && (
-        <div className='progress__rate'>
-          <img src={carat} alt='up carat' className='carat' />
-          <div className='percentage'>16%</div>
-          <div className='text__percentage'>since last month</div>
+      {/* {className === "savigns__card" && (
+        <div className="progress__rate">
+          <img src={carat} alt="up carat" className="carat" />
+          <div className="percentage">16%</div>
+          <div className="text__percentage">since last month</div>
         </div>
-      )}
+      )} */}
     </Card>
   );
 };
